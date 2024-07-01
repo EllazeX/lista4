e52018475e88dfbef6272e6d3848d75606674b96
Patryk <dsw54182student@dsw.edu.pl>
2024-07-01 21:15:02 +0200

    Add --logs flag to create log files

--- skrypt.sh
+++ skrypt.sh
@@ -4,3 +4,13 @@ if [ "$1" == "--date" ]; then
     date
 fi
 
+if [ "$1" == "--logs" ]; then
+    num_logs=${2:-100}
+    for ((i=1; i<=num_logs; i++))
+    do
+        filename="log_$i.txt"
+        echo "File number: $i" > $filename
+        echo "Script name: $0" >> $filename
+        echo "Date: $(date)" >> $filename
+    done
+fi
